0ff81bd7f651e03b4274f0407f3573a970643ebf
cristobal sandoval <cristobal.sandoval.balboa@gmail.com>
2022-04-27 23:12:15 -0400

    Item Collection

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -3,6 +3,8 @@ import { useState, useEffect } from 'react'
 import { getProducts } from '../../asyncmock'
 import ItemList from '../ItemList/ItemList'
 import { useParams } from 'react-router-dom'
+import { getDocs, collection, query , where } from 'firebase/firestore'
+import { firestoreDb } from '../../services/firebase'
 
 const ItemListContainer = () => {
     const [products, setProducts] = useState([])
@@ -10,10 +12,22 @@ const ItemListContainer = () => {
     const { categoryId } = useParams()
 
     useEffect(() => {
-        getProducts(categoryId).then(prods => {
-            setProducts(prods)
-        }).catch(error => {
-            console.log(error)
+    //    getProducts(categoryId).then(prods => {
+         //   setProducts(prods)
+      //  }).catch(error => {
+          //  console.log(error)
+        //})
+
+        const collectionRef = categoryId
+        ? query(collection(firestoreDb, 'products'), where('category', '==', categoryId))
+        : collection(firestoreDb, 'products')
+
+        getDocs(collectionRef).then(response => {
+            console.log(response)
+            const products = response.docs.map(doc =>{
+                return { id: doc.id, ...doc.data()}
+            })
+            setProducts(products)
         })
     }, [categoryId])
 
